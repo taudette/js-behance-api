86a2e2e316fe7c82db3e9fc85940a176ff586cfa
Tyler Audette <tyleraudette5@gmail.com>
2017-12-16 12:06:34 -0800

    Add dev tools to store

--- src/store.js
+++ src/store.js
@@ -4,10 +4,14 @@ import reducer from './reducers'
 
 const createAppStore = compose(
   applyMiddleware(thunkMiddleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )(createStore);
 
 export default function configureStore(initialState){
   const store = createAppStore(reducer, initialState);
   return store;
 };
+
+
+
 
